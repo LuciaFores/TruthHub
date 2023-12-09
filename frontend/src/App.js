6b510bc0dbfc20b28355dee20433b7bb349d0cb2
LuciaFores <77293908+LuciaFores@users.noreply.github.com>
2023-12-09 19:03:55 +0100

    Added first draft of daisyUI
    - Added first draft of daisyUI usage
    - Added requirements to start the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,7 @@
-import { Theme,
-        Button } from 'react-daisyui';
-import './App.css';
-
 function App() {
   return (
     <div>
-      <Theme dataTheme="synthwave">
-        <Button color="primary">Click me, dark!</Button>
-      </Theme>
-
-      <Theme dataTheme="light">
-      <Button color="primary">Click me, light!</Button>
-      </Theme>
+      <button className="btn">Hello daisyUI</button>
     </div>
   );
 }
