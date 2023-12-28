4fcd481671f7da82050b297ca89efd45f3a3f4df
LuciaFores <77293908+LuciaFores@users.noreply.github.com>
2023-12-28 19:15:41 +0100

    Update TruthHub.sol
    - modified main contract

--- backend/contract/TruthHub.sol
+++ backend/contract/TruthHub.sol
@@ -1,22 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+/// Address library provides utilities for working with addresses
+import "@openzeppelin/contracts/utils/Address.sol";
+/// Enumerable set is used to have efficient structure "stack-like"
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
+/// import openzeppelin math
+import "@openzeppelin/contracts/utils/math/Math.sol";
+
 contract TruthHub {
+    using Address for address;
+    /// Using EnumerableSet for EnumerableSet.AddressSet type
+    using EnumerableSet for EnumerableSet.AddressSet;
+
     /// *** CONTRACT VARIABLES *** ///
 
-    /// Minimum ether price needed to express a vote, it will be given back to 
-    /// either to the voter or the author of the article
-    immutable uint256 public minEtherVotePrice;
+    /// Ether price needed to express a vote, it will be given back to 
+    /// either to the voter or the author of the article and it is weighted
+    /// by the reputation
+    immutable uint256 public etherVotePrice;
 
-    /// Minimum ether price needed to publish an article, it will be given
-    /// back either to the author of the article or the voter
-    immutable uint256 public minEtherPublishPrice;
+    /// Ether price needed to publish an article, it will be given
+    /// back either to the author of the article or the voter and it is
+    /// weighted by the reputation
+    immutable uint256 public etherPublishPrice;
 
     /// Amount of platform tokens given per Ether spent
-    immutable uint256 public _amountVeriPerEther;
+    immutable uint256 public amountVeriPerEther;
 
-    /// Amount of ethers spent to vote an article
-    uint256 public etherSpentToVote;
+    /// Minimum delay needed to consider a vote valid (4 days in terms of blocks)
+    immutable uint256 public minimumBlockDelta;
+
+    /// Max delay needed to consider a vote valid (7 days in terms of blocks)
+    immutable uint256 public maximumBlockDelta;
+
+    /// Total weight vote needed to consider an vote ended
+    immutable uint256 public endWeightVote; // siccome devi moltiplicare tutto per 100 se ad esempio vuoi chiudere a 2 voti devi chiudere a 200
 
     /// Mapping needed to register the correspondences between Ethereum accounts
     /// and Nostr accounts, it is used to check if a user is also an author
@@ -24,12 +43,13 @@ contract TruthHub {
     mapping(address => byte32) public authors;
 
     /// Mapping needed to register the reputations of the users
-    /// The reputaton is a number between 0 and 100
+    /// The reputaton is a number between -0.5 and +0.5
+    /// (e.g. max reputation -> half price and double power)
     /// It is a mapping(address ethereumAddress => uint8 reputation)
     mapping(address => uint8) public reputations;
 
     /// Variable needed to save the total number of articles published
-    uint256 internal _totalArticles;
+    uint256 public totalArticles;
 
     /// Struct used to represent an article
     struct Article {
@@ -39,58 +59,55 @@ contract TruthHub {
         byte32 eventId;
         // Ethereum address of the author
         address author;
-        /// Number of upvotes collected by the article
+        /// Totalizzatore dei voti pesati positivi espressi per l'articolo
         uint256 upvotes;
-        /// Number of downvotes collected by the article
+        /// Totalizzatore dei voti pesati negativi espressi per l'articolo
         uint256 downvotes;
         /// Ethers spent to publish the article
         uint256 etherSpentToPublish;
+        /// Number of people who upvoted
+        uint256 upvoters;
+        /// Set of upvoters' addresses
+        EnumerableSet.AddressSet upvotersAddresses;
+        /// Number of people who downvoted
+        uint256 downvoters;
+        /// Set of downvoters' addresses
+        EnumerableSet.AddressSet downvotersAddresses;
         /// Minimum amount of time, in terms of blocks, that must pass
         /// in order to consider a vote valid
         /// The formula is the following:
         /// minimum_block_threshold = current_block_height + 4 days
-        immutable uint256 minimumBlockThreshold;
+        uint256 minimumBlockThreshold;
         /// Maximum amount of time, in terms of blocks, that must pass
         /// in order to consider a vote valid
         /// The formula is the following:
         /// maximum_block_threshold = current_block_height + 7 days
-        immutable uint256 maximumBlockThreshold;
-        /// Number of active voters, it is a computable variable so it 
+        uint256 maximumBlockThreshold;
+        /// Ethers spent to upvote
+        /// It is used to store how much an user has spent to upvote an article
+        /// It is a mapping (address userEthereumAddress => uint256 etherSpentToUpvote)
+        mapping (address => uint256) etherSpentToUpvote;
+        /// Ethers spent to downvote
+        /// It is used to store how much an user has spent to downvote an article
+        /// It is a mapping (address userEthereumAddress => uint256 etherSpentToDownvote)
+        mapping (address => uint256) etherSpentToDownvote;
+        /// Number of total weighted votes, it is a computable variable so it 
         /// is not stored in the struct, the formula is the following:
-        /// activeVoters = upvotes + downvotes
+        /// totalWeightVotes = upvotes + downvotes
     }
-    /// Total number of votes needed to consider an vote ended
-    immutable uint256 public endVotes;
 
     /// Mapping needed to register the articles
-    /// It is needed to map the eventId of the Nostr event to the actual
+    /// It is needed to map the articleId of the article to the actual
     /// information about the article
-    /// It is a mapping(byte32 eventId => Article article)
-    mapping(byte32 => Article) public articles;
+    /// It is a mapping(uint256 articleId => Article article)
+    mapping(uint256 => Article) public articles;
 
     /// The following two mappings may be useful or not, if they will never
     /// be used, delete them
     /// Mapping needed to register the correspondences between the eventIds
     /// and the articleIds
     /// It is a mapping(byte32 eventId => uint256 articleId)
-    mapping(byte32 => uint256) public eventIdToArticleId;
-
-    /// Mapping needed to register the correspondences between the articleIds
-    /// and the eventIds
-    /// It is a mapping(uint256 articleId => byte32 eventId)
-    mapping(uint256 => byte32) public articleIdToEventId;
-
-    /// Mapping needed to register all the articles for which an user has voted
-    /// It is a mapping(address ethereumAddress => mapping(byte32 eventId => bool voted))
-    mapping(address => mapping(byte32 => bool)) public voted;
-
-    /// Mapping needed to register the vote that an user gave to an article
-    /// It is a mapping(address ethereumAddress => mapping(byte32 eventId => bool voteExpressed))
-    mapping(address => mapping(byte32 => bool)) public voteExpressed;
-
-    /// Mapping needed to register how much an user payed to express a vote
-    /// It is a mapping(address ethereumAddress => mapping(byte32 eventId => uint256 etherSpentToVote))
-    mapping(address => mapping(byte32 => uint256)) public etherSpentToVote;
+    mapping(byte32 => uint256) public eventIdToArticleId; // metti funzione che prende uint256 e restituisce byte32 sarebbe articleIdToEventId
 
 
     /// *** CONTRACT MODIFIERS *** ///
@@ -102,19 +119,38 @@ contract TruthHub {
     }
 
     /// The following modifier checks if a vote is open; it checks if
-    /// the current block height is between the minimum and the maximum and
+    /// the current block height lower than the maximum and
     /// also if the number of active voters is less than the minimum number
     /// of votes needed to consider a vote valid
-    modifier voteOpen(byte32 eventId) {
-        require(block.number <= articles[eventId].maximumBlockThreshold && articles[eventId].upvote + articles[eventId].downvote <= endVotes, "Vote is closed");
+    modifier voteOpen(uint256 articleId) {
+        require(block.number <= articles[articleId].maximumBlockThreshold && (articles[articleId].upvotes + articles[articleId].downvotes < endWeightVote || block.number < articles[articleId].minimumBlockThreshold), "Vote is closed");
         _;
     }
 
     /// The following modifier checks if a vote is closed; it checks if
     /// the current block height is greater than the maximum or
     /// if the number of active voters is greater than the minimum number and the current block height is grater than the minimum
-    modifier voteClosed(byte32 eventId) {
-        require(block.number > articles[eventId].maximumBlockThreshold || (articles[eventId].upvote + articles[eventId].downvote >= endVotes && block.number > articles[eventId].minimumBlockThreshold), "Vote is open");
+    modifier voteClosed(uint256 articleId) {
+        require(block.number > articles[articleId].maximumBlockThreshold || (articles[articleId].upvotes + articles[articleId].downvotes >= endWeightVote && block.number >= articles[articleId].minimumBlockThreshold), "Vote is open");
+        _;
+    }
+
+    /// The following modifier checks if a user has already voted for an article
+    /// It checks if the user has already spent ether to upvote or downvote an article
+    modifier validVoter(uint256 articleId) {
+        require(articles[articleId].etherSpentToUpvote[msg.sender] == 0 && articles[articleId].etherSpentToDownvote[msg.sender] == 0, "You have already voted");
+        _;
+    }
+
+    modifier validClaimer(uint256 articleId) {
+        // Check if the msg.sender is in the majority or if there is a tie between upvotes and downvotes
+        require(containes(articles[articleId].upvotersAddresses, msg.sender) || containes(articles[articleId].downvotersAddresses, msg.sender), "You did not vote");
+        if (articles[articleId].upvotes > articles[articleId].downvotes){
+            require(containes(articles[articleId].upvotersAddresses, msg.sender), "You are not in the majority");
+        }
+        else if (articles[articleId].upvotes < articles[articleId].downvotes){
+            require(containes(articles[articleId].downvotersAddresses, msg.sender) && articles[articleId].author != msg.sender, "You are not in the majority");
+        }
         _;
     }
 
@@ -122,11 +158,17 @@ contract TruthHub {
     /// constructor function
     constructor() {
         // Initialize the immutable variables
-        minEtherVotePrice = 1000000 gwei;
-        minEtherPublishPrice = 1000000 gwei;
-        _totalArticles = 0;
-        _amountVeriPerEther = 1000; // 1 ether = 1000 veri -> 0.01 ether = 10 veri
-        endVotes = 31;
+        etherVotePrice = 1000000000000000 wei; // 0.001 ether
+        etherPublishPrice = 10000000000000000 wei; // 0.01 ether
+        amountVeriPerEther = 1000; // 1 ether = 1000 veri -> 0.01 ether = 10 veri
+        endWeightVote = 31; // placeholder value
+        minimumBlockDelta = 23040; // 4 days in terms of blocks
+        maximumBlockDelta = 40320; // 7 days in terms of blocks
+        totalArticles = 0;
+    }
+
+    /// The following function is used to receive ether
+    receive() external payable {
     }
 
     /// The following function is used to register a new author
@@ -141,27 +183,88 @@ contract TruthHub {
     /// may copy the transaction and let the transaction be mined before the original one, thus registering themselves
     /// as the ethereum account associated with that specific Nostr address
     /// The function also checks if the user is already registered as an author
-    function registerAuthor(byte32 encryptedNostrPublicKey, byte32 nostrPublicKey) external {
+    function registerAuthor(byte32 signature, byte32 nostrPublicKey) external {
         require(authors[msg.sender] == 0, "You are already an author");
-        //require(keccak256(abi.encodePacked(encryptedNostrPublicKey)) == nostrPublicKey, "Invalid cryptographic proof");
+        //require(keccak256(abi.encodePacked(signature)) == nostrPublicKey, "Invalid cryptographic proof");
         authors[msg.sender] = nostrPublicKey;
+    }
+
+    function computation(address user, uint256 actionPrice) internal view returns (uint256){
+        // se reputazione è 100 pago la metà se è 1 pago il doppio se 50 pago prezzo base
+        // 1 = 200
+        // 51 = 100
+        // 101 = 50 
+        uint256 x;
+        uint256 reputation = reputations[user];
+        if (reputation <= 51){
+            uint256 subRes;
+            uint256 mulRes;
+            // 100 + ((51 - reputation) * 2)
+            (, subRes) = trysub(51, reputation);
+            (, mulRes) = trymul(subRes, 2);
+            (, x) = tryadd(100, mulRes);
+        }
+        else{
+            (, x) = trysub(reputation, 51);
+        }
+        return muldiv(actionPrice, x, 100);
+    }
+
+    function computePublishPrice(address author) public view returns (uint256) {
+        return computation(author, etherPublishPrice);
+    }
+
+    function computeVotePrice(address author) public view returns (uint256) {
+        return computation(author, etherVotePrice);
+    }
+
+    function computeVoteWeight(address voter) public view return (uint256) {
+        // se reputazione è 100 pago la metà se è 1 pago il doppio se 50 pago prezzo base
+        // 1 = 50
+        // 51 = 100
+        // 101 = 200
+        uint256 x;
+        uint256 reputation = reputations[voter];
+        if (reputation <= 51){
+            (, x) = tryadd(reputation, 49);
+        }
+        else{
+            // 100 + ((reputation - 51) * 2)
+            uint256 subRes;
+            uint256 mulRes;
+            (, subRes) = trysub(reputation, 51);
+            (, mulRes) = trymul(subRes, 2);
+            (, x) = tryadd(100, mulRes);
+        }
+        return x;
     }
 
     /// The following function is used to publish a new article
     /// In order to do that the user must be registered as an author
     /// The function also checks if the user has enough ether to pay the
     /// minimum price needed to publish an article
-    // Once that an article is published it is automatically ready to be voted
+    /// Once that an article is published it is automatically ready to be voted
     function publishArticle(byte32 eventId) external payable onlyAuthor returns (uint256){
-        require(msg.value >= minEtherPublishPrice, "Not enough ether");
-        require(articles[eventId].eventId == 0, "Article already published");
-        articleId = _totalArticles; // può esserci l'id 0 o crea problemi con il default?
-        _totalArticles += 1;
-        author = msg.sender;
-        etherSpentToPublish = msg.value;
-        minimum_block_threshold = block.number + 23040;
-        maximum_block_threshold = block.number + 40320;
-        articles[eventId] = Article(articleId, eventId, author, 0, 0, etherSpentToPublish, minimum_block_threshold, maximum_block_threshold);
+        require(msg.value >= computePublishPrice(msg.sender), "Not enough ether");
+        require(eventIdToArticleId[eventId] == 0, "Article already published");
+        totalArticles += 1;
+        // The mappings are created empty by default
+        articles[articleId] = Article(
+            totalArticles, // articleId
+            eventId, // eventId
+            msg.sender, // author
+            0, // upvotes
+            0, // downvotes
+            msg.value, // etherSpentToPublish
+            0, // upvoters
+            EnumerableSet.AddressSet(), // upvotersAddresses
+            0, // downvoters
+            EnumerableSet.AddressSet(), // downvotersAddresses
+            block.number + minimumBlockDelta, // minimumBlockThreshold
+            block.number + maximumBlockDelta // maximumBlockThreshold
+            );
+        articles[articleId].upvotersAddresses.add(msg.sender);
+        articles[articleId].downvotersAddresses.add(msg.sender);
         return articleId;
     }
 
@@ -171,20 +274,20 @@ contract TruthHub {
     // minimum price needed to express a vote
     // The function also checks if the user has already voted for the article
     // The function also checks if the user is the author of the article
-    // Non sono sicura funzioni, non so se chiedendo il valore di una chiave che non esiste nel mapping
-    // appare il default -> CHIEDI
-    function vote (byte32 eventId, bool vote) external payable voteOpen(eventId) {
-        require(msg.value >= minEtherVotePrice, "Not enough ether");
-        require(voted[msg.sender][eventId] == false, "You have already voted");
-        require(articles[eventId].author != msg.sender, "You are the author");
-        voted[msg.sender][eventId] = true;
-        etherSpentToVote[msg.sender][eventId] = msg.value;
+    // POI AGGIUNGI IL PESO DOVUTO AI TOKENS
+    function vote (uint256 articleId, bool vote) external payable validVoter(articleId) voteOpen(articleId) {
+        require(msg.value >= computeVotePrice(msg.sender), "Not enough ether");
         if (vote) {
-            voteExpressed[msg.sender][eventId] = true;
-            articles[eventId].upvotes += 1;
-        } else {
-            // Not needed to update the mapping because the default value is false
-            articles[eventId].downvotes += 1;
+            articles[articleId].upvotes += computeVoteWeight(msg.sender);
+            articles[articleId].upvoters += 1;
+            articles[articleId].upvotersAddresses.add(msg.sender);
+            articles[articleId].etherSpentToUpvote[msg.sender] = msg.value;
+        }
+        else {
+            articles[articleId].downvotes += computeVoteWeight(msg.sender);
+            articles[articleId].downvoters += 1;
+            articles[articleId].downvotersAddresses.add(msg.sender);
+            articles[articleId].etherSpentToDownvote[msg.sender] = msg.value;
         }
     }
 
@@ -206,13 +309,13 @@ contract TruthHub {
     /// has been spent to express the vote and a proportional part of the ethers collected among the upvotes and the ethers
     /// spent to publish the article; the users that expressed an upvote loose the ether spent to express the vote
     /// and their reputation is lowered by 1
-    function claim_stake_reward (byte32 eventId) external voteClosed(eventId) {
-        if (articles[eventId].upvotes > articles[eventId].downvotes) {
-            // Checks if the msg.sender is an author
-            if (authors[msg.sender] != 0){
+    function claim_stake_reward (uint256 articleId) external validClaimer voteClosed(articleId) {
+        // MAJORITY -> UPVOTES
+        if (articles[articleId].upvotes > articles[articleId].downvotes) {
+            // Checks if the msg.sender is the author
+            if (articles[articleId].author == msg.sender){
                 // The author gets the ethers spent to publish the article and the ethers collected from the downvotes
                 // and an amount of platform tokens proportional to how much ether has been spent to publish the article
-                // Come calcolo i possibili downvote con prezzi diversi? Direi di lasciare qui solo il minimo e poi il resto se lo intasca il contract
                 payable(msg.sender).transfer(articles[eventId].etherSpentToPublish + articles[eventId].downvotes * minEtherVotePrice);
                 // The author gets an amount of platform tokens proportional to how much ether has been spent to publish the article
                 // The formula is the following:
