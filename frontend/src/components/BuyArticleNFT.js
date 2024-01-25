22b47b0268e4de56c1ca6498bee654389945ce11
LuciaFores <77293908+LuciaFores@users.noreply.github.com>
2024-01-25 01:12:00 +0100

    Fixed write
    - Fixed write function with ethers

--- frontend/src/components/BuyArticleNFT.js
+++ frontend/src/components/BuyArticleNFT.js
@@ -1,14 +1,22 @@
 import { Web3Button } from "@thirdweb-dev/react";
 import { TruthHubAddress, TruthHubAbi } from "../contracts.js";
+import { ethers } from "ethers";
 
 export default function BuyArticleNFT({articleId, nftAmount, buyPrice}) {
+
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    let truthHubContractInstance = new ethers.Contract(TruthHubAddress, TruthHubAbi, provider);
+    const signer = provider.getSigner();
+    truthHubContractInstance = truthHubContractInstance.connect(signer);
+
     return(
         <Web3Button
         contractAddress={TruthHubAddress}
         contractAbi={TruthHubAbi}
-        action={async (contract) => {
-            const overrides = {value : String(buyPrice)};
-            await contract.call("buyArticleNFT", [articleId, nftAmount], overrides);
+        action={async () => {
+            const overrides = {value : String(nftAmount * buyPrice)};
+            const buyTransaction = await truthHubContractInstance.buyArticleNFT(articleId, nftAmount, overrides);
+            await buyTransaction.wait();
         }}
         onError={(error) => alert(error)}
         >
--- frontend/src/components/ClaimRewardButton.js
+++ frontend/src/components/ClaimRewardButton.js
@@ -1,13 +1,20 @@
 import { Web3Button } from "@thirdweb-dev/react";
 import { TruthHubAddress, TruthHubAbi } from "../contracts.js";
+import { ethers } from "ethers";
 
 export default function ClaimReward({articleId}) {
+    let provider = new ethers.providers.Web3Provider(window.ethereum);
+    let truthHubContractInstance = new ethers.Contract(TruthHubAddress, TruthHubAbi, provider);
+    const signer = provider.getSigner();
+    truthHubContractInstance = truthHubContractInstance.connect(signer);
+
     return(
         <Web3Button
         contractAddress={TruthHubAddress}
         contractAbi={TruthHubAbi}
-        action={async (contract) => {
-            await contract.call("claimReward", [articleId]);
+        action={async () => {
+            const claimTransaction = await truthHubContractInstance.claimReward(articleId);
+            await claimTransaction.wait();
         }}
         onError={(error) => alert(error)}
         >
