3d5f92db4f3119cffae5d7f5129a08414abdab79
LuciaFores <77293908+LuciaFores@users.noreply.github.com>
2024-01-26 11:39:14 +0100

    Final contracts deploy
    - Final contracts deploy
    - Fixed contracts varible in the frontend

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -10,15 +10,15 @@ export default function Footer(){
                 </aside>
                 <nav>
                     <header className="footer-title">Contract Clear Code</header> 
-                    <a className="link link-hover" href="https://sepolia.etherscan.io/address/0xbCbB612746AcbD35EA9762465d3406e64D8268c6#code">TruthHub</a>
-                    <a className="link link-hover" href="https://sepolia.etherscan.io/address/0xf7026BF58464683F5e56D51Be02664D832662a4f#code">VeriToken</a>
-                    <a className="link link-hover" href="https://sepolia.etherscan.io/address/0x6eAB4fB9aB67F74800Be8DF15f08982889Bcdb32#code">ArticleNFT</a>
+                    <a className="link link-hover" href="https://sepolia.etherscan.io/address/0xbC82d0C512E7Ae6BcCdC5cC71d6AeFABD1ba49d2#code">TruthHub</a>
+                    <a className="link link-hover" href="https://sepolia.etherscan.io/address/0xa650238d8189139C2dA3013eB4F9e273D0605C2e#code">VeriToken</a>
+                    <a className="link link-hover" href="https://sepolia.etherscan.io/address/0xae3aA6F03497A8c71C6C2Adc7232b1Db39e26999#code">ArticleNFT</a>
                 </nav> 
                 <nav>
                     <header className="footer-title">Demo Contract Clear Code</header> 
-                    <a className="link link-hover" href="https://sepolia.etherscan.io/address/0xbCbB612746AcbD35EA9762465d3406e64D8268c6#code">TruthHub</a>
-                    <a className="link link-hover" href="https://sepolia.etherscan.io/address/0xf7026BF58464683F5e56D51Be02664D832662a4f#code">VeriToken</a>
-                    <a className="link link-hover" href="https://sepolia.etherscan.io/address/0x6eAB4fB9aB67F74800Be8DF15f08982889Bcdb32#code">ArticleNFT</a>
+                    <a className="link link-hover" href="https://sepolia.etherscan.io/address/0x7922157191d5a665dE92A899985B14eB67511dcC#code">TruthHub</a>
+                    <a className="link link-hover" href="https://sepolia.etherscan.io/address/0xF945Bf08F91CC3F56C71cA6CC32AcA49F6A4F5a5#code">VeriToken</a>
+                    <a className="link link-hover" href="https://sepolia.etherscan.io/address/0x659401ED0C0841f1E0460cA4b77bc51CfE466ea1#code">ArticleNFT</a>
                 </nav>
             </footer>
         </div>
